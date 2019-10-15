47b301bf1c8050a4abf9016981e655ca12204fd6
tokky <otokita@pepabo.com>
2019-10-15 11:35:58 +0900

    update script

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,5 @@ ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/dotfiles/.bash_profile $HOME/.bash_profile
 ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
 ln -sf $HOME/dotfiles/.xvimrc $HOME/.xvimrc
+ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -sf $HOME/dotfiles/.zshenv $HOME/.zshenv
